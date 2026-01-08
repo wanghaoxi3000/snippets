20af4f4744e2abe8daa1f80aa6dbc2439783cf62
wanghaoxi3000 <wanghaoxi3000@163.com>
2026-01-08 08:46:09 +0000

    update: Update clash-meta build shell

--- docker/clash-gate/build-image.sh
+++ docker/clash-gate/build-image.sh
@@ -10,16 +10,23 @@ echo "Build clash gate docker image by version: [$1] with metacubexd [$2]"
 CLASH_VERSION=$1
 METACUBEXD_VERSION=$2
 
+echo
 echo "Download clash meta version: ${CLASH_VERSION}"
-curl -LO https://github.com/MetaCubeX/mihomo/releases/download/${CLASH_VERSION}/mihomo-linux-amd64-compatible-go120-${CLASH_VERSION}.gz
-gzip -d mihomo-linux-amd64-compatible-go120-${CLASH_VERSION}.gz
-mv mihomo-linux-amd64-compatible-go120-${CLASH_VERSION} clash-meta
+curl -LO https://github.com/MetaCubeX/mihomo/releases/download/${CLASH_VERSION}/mihomo-linux-amd64-v1-${CLASH_VERSION}.gz
+gzip -d mihomo-linux-amd64-v1-${CLASH_VERSION}.gz
+mv mihomo-linux-amd64-v1-${CLASH_VERSION} clash-meta
 
+echo
 echo "Download metacubexd vesion: ${METACUBEXD_VERSION}"
 curl -LO https://github.com/MetaCubeX/metacubexd/releases/download/${METACUBEXD_VERSION}/compressed-dist.tgz
 mkdir -p config/ui
 tar -xzf compressed-dist.tgz -C config/ui
 
+echo
 echo "Build docker image"
 docker build -t clash-meta-gate:${CLASH_VERSION} .
+
+echo
+echo "Clean resource"
+rm -rf mihomo-linux-amd64-compatible-go120-${CLASH_VERSION}.gz compressed-dist.tgz clash-meta config/ui
 
